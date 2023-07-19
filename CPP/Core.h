898fce86efc96653eebb648b242ce29579fe6e85
Tsvi Zandany <tsviz@github.com>
2023-07-19 11:09:03 -0500

    Testing buffer

--- CPP/Core.h
+++ CPP/Core.h
@@ -4,5 +4,7 @@
 #include <iostream>
 
 const char *concatenateMyStringWithCppString(const char *myString);
+// add methods with cwe-787 (Out-of-bounds Write) and cwe-79 (Improper Neutralization of Input During Web Page Generation ('Cross-site Scripting'))
+const char *methodWithCWE787();
 
 #endif /* defined(__HelloCpp__Core__) */
--- Unix/compileToUnix.sh
+++ Unix/compileToUnix.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
 g++ Main.cpp ../CPP/Core.cpp -o main
-echo "Compiled, now execute using ./main"
